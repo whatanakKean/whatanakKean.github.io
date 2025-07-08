0d9fe5381343c37d1933c199863c8fccacf2f2bf
whatanakKean <85047164+whatanakKean@users.noreply.github.com>
2025-07-08 02:01:51 +0000

    fix too

--- data/dashboardsData.ts
+++ data/dashboardsData.ts
@@ -7,20 +7,10 @@ interface Dashboard {
 
 const dashboardsData: Dashboard[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
+    title: 'Cambodia Tourism Intelligence',
+    description: `Explore Cambodia's tourism data with interactive visualizations and insights.`,
     imgSrc: '/static/images/google.png',
-    href: '/dashboards/test',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    href: '/dashboards/cambodia-tourism-intelligence',
   },
 ]
 
