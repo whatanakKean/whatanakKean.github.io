2468dddf6ef1196d217828bbedbf4da20ae12c09
whatanakKean <85047164+whatanakKean@users.noreply.github.com>
2025-07-11 10:14:08 +0000

    1

--- app/dashboards/cambodia-tourism-intelligence/page.tsx
+++ app/dashboards/cambodia-tourism-intelligence/page.tsx
@@ -195,7 +195,7 @@ export default function Dashboard() {
   return (
     <div className="flex-grow overflow-auto bg-gray-50">
       <div className="max-w-8xl mx-auto flex h-16 w-full items-center justify-between border-b border-gray-200 bg-white px-6">
-        <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+        <h1 className="text-2xl font-semibold text-gray-900">Cambodia Tourism Intelligence</h1>
       </div>
 
       <main className="max-w-8xl mx-auto space-y-6 p-6">
